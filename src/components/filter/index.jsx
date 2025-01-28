0771d72274c8e2ba526af0240bf7fb7080ebc6fe
Ram Sharan Rimal <rsr0012@uah.edu>
2025-01-27 22:55:27 -0600

    remove regions from filter component

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -2,40 +2,21 @@ import { useState, useEffect } from "react";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import moment from "moment";
 
-export function FilterByDate({regions, vizItems, setFilteredRegions,setFilteredSelectedVizItems }) {
+export function FilterByDate({ vizItems,setFilteredVizItems }) {
     const [startDate, setStartDate] = useState(moment("2018-01-01"));
     const [endDate, setEndDate] = useState(() => moment());
 
     useEffect(() => {
-        if (!regions.length) return;
-
-        const filteredRegions = []
-        // only filter ones where: startDate <= vizItem.date <= endDate
-        for (let i=0; i < regions.length; i++) {
-            const region = regions[i];
-            const regionStartDatetime = moment(region.startDate);
-            const regionEndDatetime = moment(region.endDate);
-            if (regionStartDatetime.isSameOrAfter(startDate) && regionEndDatetime.isSameOrBefore(endDate)) {
-                filteredRegions.push(region);
-            }
-        }
-        setFilteredRegions(filteredRegions);
-
-        if (!vizItems || !vizItems.length) return;
-
-        const filteredVizItems = []
-        // only filter ones where: startDate <= vizItem.date <= endDate
-        for (let i=0; i < vizItems.length; i++) {
-            const vizItem = vizItems[i];
+        if (!vizItems.length) return;
+        const filteredVizItems = vizItems.filter(vizItem => {
             const vizItemStartDatetime = moment(vizItem.startDate);
             const vizItemEndDatetime = moment(vizItem.endDate);
-            if (vizItemStartDatetime.isSameOrAfter(startDate) && vizItemEndDatetime.isSameOrBefore(endDate)) {
-                filteredVizItems.push(vizItem);
-            }
-        }
-        setFilteredSelectedVizItems(filteredVizItems);
-
-    }, [regions, startDate, endDate, setFilteredRegions, vizItems, setFilteredSelectedVizItems]);
+             if (vizItemStartDatetime.isSameOrAfter(startDate) && vizItemEndDatetime.isSameOrBefore(endDate)) {
+                return vizItem;
+            }else return null
+        });
+        setFilteredVizItems(filteredVizItems.map(vizItem => vizItem));
+    }, [ startDate, endDate,  vizItems, setFilteredVizItems]);
 
     return (
         <>

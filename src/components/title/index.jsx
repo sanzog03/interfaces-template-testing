02f4ce98b58853b368373b3cbf990dcfdb87af08
Ram Sharan Rimal <rsr0012@uah.edu>
2025-01-27 22:55:45 -0600

    separte title components

--- src/components/title/index.jsx
+++ src/components/title/index.jsx
@@ -1,21 +1,9 @@
 import React from "react";
-import styled from "styled-components";
 
-import { Paper, Typography, Divider } from "@mui/material";
+import {  Typography, Divider } from "@mui/material";
 import "./index.css";
-import { Search } from "../../components/search";
-import { FilterByDate } from '../../components/filter';
-import { VizItemAnimation } from '../../components/plumeAnimation';
 
- const HorizontalLayout = styled.div`
-    width: 90%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    margin: 12px;
-`;
- 
- const Title = ({title, description}) => {
+ export const Title = ({ title,description  }) => {
     return (
         <>
             <Typography
@@ -41,43 +29,8 @@ import { VizItemAnimation } from '../../components/plumeAnimation';
                     borderWidth: "1px",
                     margin: "0.3rem 1.4rem",
                 }}
-            /> 
+            />
+            
         </>
     )
  }
-
-const InfoCardWithFilters = ({
-        title,
-        description,
-        vizItemIds,
-        handleSelectedVizItemSearch,
-        regions,
-        selectedVizItems,
-        setFilteredRegions,
-        setFilteredSelectedVizItems,
-        vizItemsForAnimation 
-    }) => {
-    return (
-        <Paper className="title-container">
-            <Title title={title} description={description}/>
-            <HorizontalLayout>
-                <Search
-                    ids={vizItemIds}
-                    handleSelectedVizItemSearch={handleSelectedVizItemSearch}
-                ></Search>
-            </HorizontalLayout>
-            <HorizontalLayout>
-                <FilterByDate
-                    regions={regions}
-                    vizItems={selectedVizItems}
-                    setFilteredRegions={setFilteredRegions}
-                    setFilteredSelectedVizItems={setFilteredSelectedVizItems}
-                />
-            </HorizontalLayout>
-            <HorizontalLayout>
-                <VizItemAnimation vizItems={vizItemsForAnimation} />
-            </HorizontalLayout>
-        </Paper>
-    )
-}
-export default InfoCardWithFilters

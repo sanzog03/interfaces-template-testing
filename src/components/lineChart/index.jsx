ee245a97e7ff712b3d85db7a31b1f23d4cf753c8
Sanjog Thapa <sanzog03@gmail.com>
2025-02-14 10:41:58 -0600

    prettier fix and revert the chart context

--- src/components/lineChart/index.jsx
+++ src/components/lineChart/index.jsx
@@ -1,28 +1,15 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-import { useChart } from "../../context/chartContext";
+import { useChart } from '../../context/chartContext';
 
 export const LineChart = ({ index, data, labels }) => {
-    // data should be an array of vector points
-    // labels should be an array of the vector labels
+  // data should be an array of vector points
+  // labels should be an array of the vector labels
+  const { chart } = useChart();
 
-    const { chart } = useChart();
-    useEffect(() => {
-        if (!chart || !data.length || !labels.length) return;
+  useEffect(() => {
+    if (!chart || !data.length || !labels.length) return;
+  });
 
-        if (!chart.data.datasets[index]) {
-            chart.data.datasets[index] = {
-                label: `Dataset ${index + 1}`,
-                data: [],
-                borderColor: "red",
-                borderWidth: 2,
-            };
-        }
-
-        chart.data.labels = labels;
-        chart.data.datasets[index].data = data;
-        chart.update();
-    }, [chart, data, labels]);
-
-    return null;
-}
+  return null;
+};
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -3,13 +3,24 @@ import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 import Box from '@mui/material/Box';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import { Title } from '../../components/title';
-import MainChart from "../../components/mainChart";
-import { LineChart } from "../../components/lineChart";
+import MainChart from '../../components/mainChart';
+import { LineChart } from '../../components/lineChart';
 
-import "./index.css";
+import './index.css';
 
-export function Dashboard({ stations, selectedStationId, setSelectedStationId, ghg, agency, region, stationCode, setSelectedGHG, zoomLevel, stationMetadata }) {
-  const [ displayChart, setDisplayChart ] = useState(false);
+export function Dashboard({
+  stations,
+  selectedStationId,
+  setSelectedStationId,
+  ghg,
+  agency,
+  region,
+  stationCode,
+  setSelectedGHG,
+  zoomLevel,
+  stationMetadata,
+}) {
+  const [displayChart, setDisplayChart] = useState(false);
 
   useEffect(() => {
     if (selectedStationId) {
@@ -25,16 +36,52 @@ export function Dashboard({ stations, selectedStationId, setSelectedStationId, g
         {
           type: 'line',
           dataPoints: [
-            10, 20, 30, 25, 50, 60, 55, 70, 65, 80, 90, 85, 100, 110, 105, 120, 125, 140, 
-            135, 150, 160, 155, 170, 180, 175, 190, 200, 195, 210, 220, 215, 230, 240, 
-            235, 250, 260, 255, 270, 280, 275, 290, 300
+            10, 20, 30, 25, 50, 60, 55, 70, 65, 80, 90, 85, 100, 110, 105, 120,
+            125, 140, 135, 150, 160, 155, 170, 180, 175, 190, 200, 195, 210,
+            220, 215, 230, 240, 235, 250, 260, 255, 270, 280, 275, 290, 300,
           ],
           dataLegend: 'Sales by Month',
           labels: [
-            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-            'Jan', 'Feb', 'Mar', 'Apr'
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec',
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec',
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec',
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
           ],
           yAxisDesc: '',
           xAxisDesc: 'Observation Date/Time (UTC)',
@@ -43,69 +90,99 @@ export function Dashboard({ stations, selectedStationId, setSelectedStationId, g
         {
           type: 'bar',
           dataPoints: [
-            10, 20, 30, 25, 50, 60, 55, 70, 65, 80, 90, 85, 100, 110, 105, 120, 125, 140, 
-            135, 150, 160, 155, 170, 180, 175, 190, 200, 195, 210, 220, 215, 230, 240, 
-            235, 250, 260, 255, 270, 280, 275, 290, 300
+            10, 20, 30, 25, 50, 60, 55, 70, 65, 80, 90, 85, 100, 110, 105, 120,
+            125, 140, 135, 150, 160, 155, 170, 180, 175, 190, 200, 195, 210,
+            220, 215, 230, 240, 235, 250, 260, 255, 270, 280, 275, 290, 300,
           ],
           dataLegend: 'Sales by Month',
           labels: [
-            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-            'Jan', 'Feb', 'Mar', 'Apr'
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec',
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec',
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec',
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
           ],
           yAxisDesc: 'Sales',
           xAxisDesc: '',
           color: '#208520',
         },
       ],
-      style: { width: '100%', height: '100%' }
+      style: { width: '100%', height: '100%' },
     },
   ];
-  
+
   return (
-    <Box className="fullSize" style={{ backgroundColor: 'gray' }}>
-        <Title ghg={ghg} agency={agency} region={region}/>
-        <img src={process.env.PUBLIC_URL + "/nist.png"} alt="NIST" className='logo'/>
-        <PanelGroup direction='vertical' className='panel-wrapper'>
+    <Box className='fullSize' style={{ backgroundColor: 'gray' }}>
+      <Title ghg={ghg} agency={agency} region={region} />
+      <img
+        src={process.env.PUBLIC_URL + '/nist.png'}
+        alt='NIST'
+        className='logo'
+      />
+      <PanelGroup direction='vertical' className='panel-wrapper'>
+        <Panel
+          id='map-panel'
+          maxSize={100}
+          defaultSize={100}
+          minSize={25}
+          className='panel'
+          order={1}
+        >
+          <div id='dashboard-map-container'></div>
+        </Panel>
+        <>
+          <PanelResizeHandle className='resize-handle'>
+            <DragHandleIcon title='Resize' />
+          </PanelResizeHandle>
           <Panel
-            id='map-panel'
-            maxSize={100}
-            defaultSize={100}
-            minSize={25}
-            className='panel'
-            order={1}
+            id='chart-panel'
+            maxSize={75}
+            minSize={40}
+            className='panel panel-timeline'
+            order={2}
           >
-            <div id="dashboard-map-container">
-              
-            </div>
+            <MainChart>
+              <LineChart index={0} data={[1, 2, 3]} labels={['a', 'b', 'c']} />
+              <LineChart index={1} data={[5, 1, 2]} labels={['a', 'b', 'c']} />
+            </MainChart>
           </Panel>
-                <>
-                  <PanelResizeHandle className='resize-handle'>
-                    <DragHandleIcon title="Resize"/>
-                  </PanelResizeHandle>
-                  <Panel
-                    id='chart-panel'
-                    maxSize={75}
-                    minSize={40}
-                    className='panel panel-timeline'
-                    order={2}
-                  >
-                    <MainChart>
-                      <LineChart
-                        index={0}
-                        data={[1,2,3]}
-                        labels={["a", "b", "c"]}
-                      />
-                      <LineChart
-                        index={1}
-                        data={[5,1,2]}
-                        labels={["a", "b", "c"]}
-                      />
-                    </MainChart>
-                  </Panel>
-              </>
-        </PanelGroup>
+        </>
+      </PanelGroup>
     </Box>
   );
 }

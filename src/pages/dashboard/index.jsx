9df383c86d7a2feb8a0b0f6fdddf1b0209039abe
Sandesh Pandey <sandeshsinbox@gmail.com>
2025-02-18 10:35:26 -0600

    Update landing page

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,105 +1,288 @@
-import React, { useEffect, useState } from 'react';
-import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import React, { useEffect, useState, useRef } from 'react';
 import Box from '@mui/material/Box';
-import DragHandleIcon from '@mui/icons-material/DragHandle';
-import { Title } from '../../components/title';
-import MainChart from '../../components/mainChart';
-import { LineChart } from '../../components/lineChart';
-import { ChartInstruction, ChartTools, ChartToolsLeft, ChartToolsRight,
-  ChartTitle, DataAccessTool, ZoomResetTool, CloseButton } from '../../components/chartComponents';
+import Paper from '@mui/material/Paper';
+
+import {
+  MainMap,
+  MarkerFeature,
+  VisualizationLayers,
+  ColorBar,
+  LoadingSpinner,
+  PersistentDrawerRight,
+  Title,
+  MapControls,
+  MapZoom,
+  Search,
+  FilterByDate,
+  VizItemAnimation,
+} from '@components';
+
+import styled from 'styled-components';
 
 import './index.css';
 
+const TITLE = 'GOES Methane Plume Viewer';
+const DESCRIPTION =
+  'The Geostationary Operational Environmental Satellites collect \
+images of the surface every 5 minutes. Only very large emission events can be detected, \
+but plume expansion is easy to see over time. More plumes will be added soon.';
+
+const HorizontalLayout = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 12px;
+`;
 export function Dashboard({
-  stations,
-  selectedStationId,
-  setSelectedStationId,
-  ghg,
-  agency,
-  region,
-  stationCode,
-  setSelectedGHG,
-  zoomLevel,
-  stationMetadata,
   data,
-  labels
+  dataTree,
+  metaDataTree,
+  collectionMeta,
+  vizItemMetaData,
+  zoomLocation,
+  setZoomLocation,
+  zoomLevel,
+  setZoomLevel,
+  loadingData,
 }) {
-  const [displayChart, setDisplayChart] = useState(true);
+  // states for data
+  const [regions, setRegions] = useState([]); // store all available regions
+  const [vizItems, setVizItems] = useState([]); // store all available visualization items
+  const [selectedRegionId, setSelectedRegionId] = useState(''); // region_id of the selected region (marker)
+  const prevSelectedRegionId = useRef(''); // to be able to restore to previously selected region.
+  const [selectedVizItems, setSelectedVizItems] = useState([]); // all visualization items for the selected region (marker)
+  const [hoveredVizLayerId, setHoveredVizLayerId] = useState(''); // vizItem_id of the visualization item which was hovered over
+  const [filteredVizItems, setFilteredVizItems] = useState([]); // visualization items for the selected region with the filter applied
+
+  const [vizItemIds, setVizItemIds] = useState([]); // list of vizItem_ids for the search feature.
+  const [vizItemsForAnimation, setVizItemsForAnimation] = useState([]); // list of subdaily_visualization_item used for animation
+  const [collectionMetadata, setCollectionMetadata] = useState({});
+  const [showVisualizationLayers, setShowVisualizationLayers] = useState(true);
+  const [showMarkerFeature, setShowMarkerFeature] = useState(true);
+  const [visualizationLayers, setVisualizationLayers] = useState(true);
+
+  //color map
+  const [VMAX, setVMAX] = useState(100);
+  const [VMIN, setVMIN] = useState(-92);
+  const [colormap, setColormap] = useState('default');
+  const [assets, setAssets] = useState('rad');
+
+  // states for components/controls
+  const [openDrawer, setOpenDrawer] = useState(false);
+  // console.log("data", data);
+  const collectionId = data[0]?.collection;
+  // console.log({dataTree})
+
+  // handler functions
+  const handleSelectedVizItem = (vizItemId) => {
+    if (
+      !dataTree.current ||
+      !Object.keys(dataTree.current).length ||
+      !vizItemId
+    )
+      return;
+    setShowVisualizationLayers(true); // all the available visualization items  layers should be visible when region is selected
+    prevSelectedRegionId.current = vizItemId;
+    const regionIdFromVizId = vizItemId?.split('_')[3];
+    setSelectedRegionId(regionIdFromVizId); // an useEffect handles it further
+    const region = dataTree.current[regionIdFromVizId];
+
+    setZoomLocation(region.location);
+    setZoomLevel(null); // take the default zoom level
+    setOpenDrawer(true);
+    setSelectedVizItems([]); // reset the visualization items shown, to trigger re-evaluation of selected visualization item
+  };
+
+  const handleSelectedVizLayer = (vizLayerId) => {
+    if (!vizItems || !vizLayerId) return;
+    const vizItem = vizItems[vizLayerId];
+    const { location } = vizItem;
+    handleSelectedVizItemSearch(vizLayerId);
+    handleAnimationReady(vizLayerId);
+    setZoomLocation(location);
+    setZoomLevel(null); // take the default zoom level
+    setSelectedRegionId(''); //to reset the visualization item that was shown
+  };
+
+  const handleAnimationReady = (vizItemId) => {
+    // will make the visualization item ready for animation.
+    if (!vizItems || !vizItemId) return;
+
+    const vizItem = vizItems[vizItemId];
+    setVizItemsForAnimation(vizItem.subDailyPlumes);
+    // just clear the previous visualization item layers and not the cards
+    setShowVisualizationLayers(false);
+  };
+
+  const handleSelectedVizItemSearch = (vizItemId) => {
+    // will focus on the visualization item along with its visualization item metadata card
+    // will react to update the metadata on the sidedrawer
+    if (!vizItems || !vizItemId) return;
+    const vizItem = vizItems[vizItemId];
+    const location = vizItem?.geometry?.coordinates[0][0];
+    console.log({ location });
+    setSelectedVizItems([vizItem]);
+    setOpenDrawer(true);
+    setZoomLocation(location);
+    setZoomLevel(null); // take the default zoom level
+    setSelectedRegionId(''); //to reset the visualization item that was shown
+    setVizItemsForAnimation([]); // to reset the previous animation
+  };
+
+  const handleResetHome = () => {
+    setSelectedRegionId('');
+    setHoveredVizLayerId('');
+    setFilteredVizItems([]);
+    setVizItemsForAnimation([]);
+    setOpenDrawer(false);
+    setZoomLevel(4);
+    setZoomLocation([-98.771556, 32.967243]);
+  };
+
+  // const handleResetToSelectedRegion = () => {
+  //   setHoveredVizItemId("");
+  //   setVizItemsForAnimation([]);
+  //   if (!prevSelectedRegionId.current) {
+  //     return handleResetHome();
+  //   }
+  //   handleSelectedVizItem(prevSelectedRegionId.current);
+  // }
+
+  // Component Effects
+  useEffect(() => {
+    if (!dataTree.current) return;
+
+    const vizItems = {}; // visualization_items[string] = visualization_item
+    const regions = []; // string[]
+    const vizItemIds = []; // string[] // for search
+    const testData = data.slice(0, 10);
+    // Object.keys(dataTree.current).forEach(region => {
+    //   regions.push(dataTree.current[region]);
+    //   dataTree.current[region].plumes.forEach(vizItem => {
+    //     // check what visualization_item is in dataModels.ts
+    //     vizItems[vizItem.id] = vizItem;
+    //     vizItemIds.push(vizItem.id);
+    //   });
+    // });
+
+    testData.forEach((items) => {
+      vizItems[items.id] = items;
+      vizItemIds.push(items.id);
+    });
+    // console.log({vizItems})
+    setVizItems(vizItems);
+    setRegions(regions);
+    setVizItemIds(vizItemIds); // for search
+    // the reference to datatree is in current, so see changes with respect to that
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataTree.current]);
 
   useEffect(() => {
-    if (selectedStationId) {
-      setDisplayChart(true);
-    }
-  }, [selectedStationId]); // only on selectedStationId prop change
+    if (!dataTree.current || !selectedRegionId) return;
+    const currentRegion = dataTree.current[selectedRegionId];
+    const visualizationLayers = currentRegion.plumes.map(
+      (layer) => layer.representationalPlume
+    );
+    setVisualizationLayers(visualizationLayers);
+    setSelectedVizItems(visualizationLayers);
+    setVizItemsForAnimation([]); // reset the animation
+    setShowVisualizationLayers(true); // all the available visualization items layers should be visible when region is selected
+    // the reference to datatree is in current, so see changes with respect to that
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataTree.current, selectedRegionId]);
 
-  const handleClose = () => {
-    setDisplayChart(false);
-  }
+  useEffect(() => {
+    const colormap = collectionMeta?.renders?.dashboard?.colormap_name;
+    const rescaleValues = collectionMeta?.renders?.dashboard?.rescale;
+    const VMIN = rescaleValues && rescaleValues[0][0];
+    const VMAX = rescaleValues && rescaleValues[0][1];
+    setVMIN(VMIN);
+    setVMAX(VMAX);
+    setColormap(colormap);
+  }, [collectionMeta]);
 
+  const onFilteredVizItems = (filteredVizItems) => {
+    //   setFilteredVizItems(filteredVizItems);
+    //   // console.log({ filteredVizItems });
+  };
+  // JSX
   return (
-    <Box className='fullSize' style={{ }}>
-      <Title ghg={ghg} agency={agency} region={region} />
-    
-      <PanelGroup direction='vertical' className='panel-wrapper'>
-        <Panel
-          id='map-panel'
-          maxSize={100}
-          defaultSize={100}
-          minSize={25}
-          className='panel'
-          order={1}
-        >
-          <div id='dashboard-map-container'></div>
-        </Panel>
-        {displayChart && (
-          <>
-            <PanelResizeHandle className='resize-handle'>
-              <DragHandleIcon title='Resize' />
-            </PanelResizeHandle>
+    <Box className='fullSize'>
+      <div id='dashboard-map-container'>
+        <MainMap>
+          <Paper className='title-container'>
+            <Title title={TITLE} description={DESCRIPTION} />
+            <div className='title-content'>
+              <HorizontalLayout>
+                <Search
+                  vizItems={Object.keys(vizItems).map((key) => vizItems[key])}
+                  onSelectedVizItemSearch={handleSelectedVizItemSearch}
+                ></Search>
+              </HorizontalLayout>
+              <HorizontalLayout>
+                <FilterByDate
+                  vizItems={Object.keys(vizItems).map((key) => vizItems[key])}
+                  onFilteredVizItems={onFilteredVizItems}
+                />
+              </HorizontalLayout>
+              <HorizontalLayout>
+                <VizItemAnimation
+                  VMIN={VMIN}
+                  VMAX={VMAX}
+                  colormap={colormap}
+                  assets={assets}
+                  vizItems={Object.keys(vizItems).map((key) => vizItems[key])}
+                />
+              </HorizontalLayout>
+            </div>
+          </Paper>
 
-            <Panel
-              id='chart-panel'
-              maxSize={75}
-              minSize={40}
-              className='panel panel-timeline'
-              order={2}
-            >
-              <MainChart>
-                  {/* Instructions and Tools container */}
-                  <ChartTools>
-                    <ChartToolsLeft>
-                      <ChartInstruction />
-                    </ChartToolsLeft>
-                    <ChartToolsRight>
-                      <DataAccessTool dataAccessLink={'https://www.google.com'} />
-                      <ZoomResetTool />
-                      <CloseButton handleClose={handleClose} />
-                    </ChartToolsRight>
-                  </ChartTools>
-
-
-                  {/* Main chart container */}
-                    <ChartTitle>My Chart</ChartTitle>
-                    <LineChart
-                      data={[1, 2, 3]}
-                      labels={['a', 'b', 'c']}
-                      legend={"daily insitu"}
-                      labelX={"date-time"}
-                      labelY={"concentration"}
-                      index={0}
-                    />
-                    <LineChart
-                      data={[2, 5, 6]}
-                      labels={['d', 'e', 'f']}
-                      labelX={"date-time"}
-                      color={'#878700'}
-                      index={1}
-                    />
-              </MainChart>
-            </Panel>
-          </>
-        )}
-      </PanelGroup>
+          <MapZoom zoomLocation={zoomLocation} zoomLevel={zoomLevel} />
+          <MapControls
+            openDrawer={openDrawer}
+            setOpenDrawer={setOpenDrawer}
+            handleResetHome={handleResetHome}
+            // handleResetToSelectedRegion={handleResetToSelectedRegion}
+          />
+          <MarkerFeature
+            vizItems={Object.keys(vizItems).map((key) => vizItems[key])}
+            onSelectVizItem={handleSelectedVizItem}
+          ></MarkerFeature>
+          <VisualizationLayers
+            vizItems={visualizationLayers}
+            VMIN={VMIN}
+            VMAX={VMAX}
+            colormap={colormap}
+            assets={assets}
+            handleLayerClick={handleSelectedVizLayer}
+            hoveredVizLayerId={hoveredVizLayerId}
+            setHoveredVizLayerId={setHoveredVizLayerId}
+          />
+        </MainMap>
+        <PersistentDrawerRight
+          open={openDrawer}
+          setOpen={setOpenDrawer}
+          selectedVizItems={filteredVizItems}
+          vizItemMetaData={vizItemMetaData}
+          metaDataTree={metaDataTree}
+          collectionId={collectionId}
+          vizItemsMap={vizItems}
+          handleSelectedVizItems={handleSelectedVizLayer}
+          hoveredVizItemId={hoveredVizLayerId}
+          setHoveredVizItemId={setHoveredVizLayerId}
+        />
+      </div>
+      {VMAX && (
+        <ColorBar
+          label={'Methane Column Enhancement (mol/m²)'}
+          VMAX={VMAX}
+          VMIN={VMIN}
+          colormap={colormap}
+          STEPSIZE={1}
+        />
+      )}
+      {loadingData && <LoadingSpinner />}
     </Box>
   );
 }

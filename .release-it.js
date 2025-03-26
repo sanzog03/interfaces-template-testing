21247dfda147221a77e4d211c179305c6a40390c
Sanjog Thapa <sanzog03@gmail.com>
2025-03-26 14:19:39 -0500

    fix: release note title change to be on same line

--- .release-it.js
+++ .release-it.js
@@ -77,6 +77,6 @@ module.exports = {
 
 function getReleaseNotes (context) {
   if (!context || !context || !context.changelog) return "";
-  const changelog = `## What's changed \n` + context.changelog;
+  const changelog = `## What's changed on v` + context.changelog;
   return changelog;
 }

4072f75f179c3d222d8705f12a9ee81a3aa40667
Sanjog Thapa <sanzog03@gmail.com>
2025-03-26 21:20:31 -0500

    feat: 1. add pre-release capability with new scripts 2. use conventional changelog to recommmend version bump as well as release notes 3. removed custom release-it plugin

--- .release-it.js
+++ .release-it.js
@@ -2,19 +2,19 @@ const debug = process.argv.includes('--debug');
 
 module.exports = {
   hooks: {
-      'before:init': ["git pull"],
-      'after:release': 'echo "VERSION_NUMBER=v${version}" >> "$GITHUB_OUTPUT"'
+    'after:bump': 'yarn && yarn build-lib',
+    'after:release': 'echo "VERSION_NUMBER=v${version}" >> "$GITHUB_OUTPUT"'
   },
   git: {
     release: debug ? false : true,
-    requireBranch: ["main", "develop"],
+    requireBranch: ["main", "develop", "GHGC-201/custom-interfaces-library"],
     commitMessage: "chore: release v${version}",
     tagName: 'v${version}',
     tagAnnotation: 'Release v${version}',
     pushArgs: ['--follow-tags'],
     requireCleanWorkingDir: debug ? false : true,
     requireUpstream: debug ? false : true,
-    changelog: 'git log --pretty=format:%s ${latestTag}...HEAD' // Output will be passed to releaseNotes context.changelog
+    // changelog: 'git log --pretty=format:%s ${latestTag}...HEAD' // this is overridden by the @release-it/conventional-changelog's changelog
   },
   github: {
     release: debug ? false : true,
@@ -29,50 +29,53 @@ module.exports = {
     registry: "https://registry.npmjs.org"
   },
   plugins: {
-    './release-it-plugins/recommended-bump/index.mjs': {}
+    "@release-it/conventional-changelog": {
+      "preset": {
+        "name": "conventionalcommits",
+        "types": [
+          {
+            "type": "feat",
+            "section": '🎉 Features',
+          },
+          {
+            "type": "fix",
+            "section": '🐛 Fixes',
+          },
+          {
+            "type": "docs",
+            "section": '🚀 Improvements',
+          },
+          {
+            "type": "ci",
+            "section": '🚀 Improvements',
+          },
+          {
+            "type": "test",
+            "section": '🚀 Improvements',
+          },
+          {
+            "type": "refactor",
+            "section": '🚀 Improvements',
+          },
+          {
+            "type": "chore",
+            "section": '🚀 Improvements',
+          },
+          {
+            "type": "revert",
+            "section": '🐛 Fixes'
+          },
+        ]
+      },
+      "infile": false,
+    }
   }
 }
 
 // helpers
 
-function getReleaseNotes (context) {
-  const logs = context.changelog.split('\n');
-  const groupedCommits = groupCommitsByCategory(logs);
-  const title = `## What's changed \n`;
-  const changelog = Object.entries(groupedCommits).map(([prefix, commits]) => {
-    if (commits.length > 0) {
-      return `### ${prefix}\n ${commits.map((c) => "* "+c).join('\n')}`;
-    }
-  }).join('\n');
-  return title + changelog;
-}
-
-function groupCommitsByCategory (logs) {
-  // return categorized logs made during a PR.
-  const grouped = {};
-  const prefixes = {
-    feat: '🎉 Features',
-    fix: '🐛 Fixes',
-    docs: '🚀 Improvements',
-    ci: '🚀 Improvements',
-    test: '🚀 Improvements',
-    refactor: '🚀 Improvements',
-    chore: '🚀 Improvements',
-    revert: '🐛 Fixes'
-  };
-
-  Object.values(prefixes).forEach((category) => {
-    grouped[category] = [];
-  });
-
-  Object.entries(prefixes).forEach(([prefix, category]) => {
-    // from all the logs, only takes in the log that was put in during a pull request
-    const regex = new RegExp(
-      `^(((Initial commit)|(Merge [^\r\n]+(\s)[^\r\n]+((\s)((\s)[^\r\n]+)+)*(\s)?)|^((${prefix})(\([\w\-]+\))?!?: [^\r\n]+((\s)((\s)[^\r\n]+)+)*))(\s)?)$`
-    );
-    const matches = logs.filter(l => l.match(regex));
-    grouped[category] = [...matches, ...grouped[category]];
-  });
-
-  return grouped;
+function getReleaseNotes(config) {
+  if (!config || !config || !config.changelog) return "🦗";
+  const changelog = `## What's changed on version:\n` + context.changelog;
+  return changelog;
 }

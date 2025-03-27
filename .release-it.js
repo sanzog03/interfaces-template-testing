3374bd6201ade587c774b3bfed48bd306b3d49dd
Sanjog Thapa <sanzog03@gmail.com>
2025-03-26 20:09:03 -0500

    feat! remove type from conventional commit plugin

--- .release-it.js
+++ .release-it.js
@@ -33,40 +33,40 @@ module.exports = {
     "@release-it/conventional-changelog": {
       "preset": {
         "name": "conventionalcommits",
-        "types": [
-          {
-            "type": "feat",
-            "section": '🎉 Features',
-          },
-          {
-            "type": "fix",
-            "section": '🐛 Fixes',
-          },
-          {
-            "type": "docs",
-            "section": '🚀 Improvements',
-          },
-          {
-            "type": "ci",
-            "section": '🚀 Improvements',
-          },
-          {
-            "type": "test",
-            "section": '🚀 Improvements',
-          },
-          {
-            "type": "refactor",
-            "section": '🚀 Improvements',
-          },
-          {
-            "type": "chore",
-            "section": '🚀 Improvements',
-          },
-          {
-            "type": "revert",
-            "section": '🐛 Fixes'
-          },
-        ]
+        // "types": [
+        //   {
+        //     "type": "feat",
+        //     "section": '🎉 Features',
+        //   },
+        //   {
+        //     "type": "fix",
+        //     "section": '🐛 Fixes',
+        //   },
+        //   {
+        //     "type": "docs",
+        //     "section": '🚀 Improvements',
+        //   },
+        //   {
+        //     "type": "ci",
+        //     "section": '🚀 Improvements',
+        //   },
+        //   {
+        //     "type": "test",
+        //     "section": '🚀 Improvements',
+        //   },
+        //   {
+        //     "type": "refactor",
+        //     "section": '🚀 Improvements',
+        //   },
+        //   {
+        //     "type": "chore",
+        //     "section": '🚀 Improvements',
+        //   },
+        //   {
+        //     "type": "revert",
+        //     "section": '🐛 Fixes'
+        //   },
+        // ]
       },
       "infile": false
     }
